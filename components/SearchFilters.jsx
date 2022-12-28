5bbedab5214caebd67c19a2781a8276cde70687c
Olasunkanmi56 <olasunkanmiolamide46@gmail.com>
2022-12-28 21:13:49 +0100

    first commit

--- components/SearchFilters.jsx
+++ components/SearchFilters.jsx
@@ -1,47 +1,63 @@
-import {useState, useEfect} from "react"
-import { Flex, Select, Box, Text, Input, Spacer, Icon, Button} from "@chakra-ui/react"
-import { useRouter } from "next/router"
-import  {MdCancel} from "react-icons/md"
-import noresult from "../assets/images/noresult.svg"
-import  Image from "next/image"
-import {filterData, getFilterValues} from "../utils/filterData"
-const SeacrhFilters = () => {
-    const [filters] = useState(filterData);
-    const [searchTerm, setSearchTerm] = useState('');
-    const [locationData, setLocationData] = useState();
-    const [showLocations, setShowLocations] = useState(false);
-    const [loading, setLoading] = useState(false);
-    const router = useRouter();
-    const searchProperties = (filterValues) => {
-        const path = router.pathname;
-        const { query } = router;
-    
-        const values = getFilterValues(filterValues)
-    
-        values.forEach((item) => {
-          if(item.value && filterValues?.[item.name]) {
-            query[item.name] = item.value
-          }
-        })
-    
-        router.push({ pathname: path, query: query });
+import { useEffect, useState } from 'react';
+import { Flex, Select, Box, Text, Input, Spinner, Icon, Button } from '@chakra-ui/react';
+import { useRouter } from 'next/router';
+import { MdCancel } from 'react-icons/md';
+import Image from 'next/image';
+
+import { filterData, getFilterValues } from '../utils/filterData';
+import { baseUrl, fetchApi } from '../utils/fetchApi';
+import noresult from '../assets/images/noresult.svg';
+
+export default function SearchFilters() {
+  const [filters] = useState(filterData);
+  const [searchTerm, setSearchTerm] = useState('');
+  const [locationData, setLocationData] = useState();
+  const [showLocations, setShowLocations] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const router = useRouter();
+
+  const searchProperties = (filterValues) => {
+    const path = router.pathname;
+    const { query } = router;
+
+    const values = getFilterValues(filterValues)
+
+    values.forEach((item) => {
+      if(item.value && filterValues?.[item.name]) {
+        query[item.name] = item.value
+      }
+    })
+
+    router.push({ pathname: path, query: query });
+  };
+
+  useEffect(() => {
+    if (searchTerm !== '') {
+      const fetchData = async () => {
+        setLoading(true);
+        const data = await fetchApi(`${baseUrl}/auto-complete?query=${searchTerm}`);
+        setLoading(false);
+        setLocationData(data?.hits);
       };
-    return (
-        <Flex bg="gray.100" p="4" justifyContent="center" flexWrap="wrap">
-             {filters.map((filter) => (
-                <Box key={filter.queryName}>
-                    <Select 
-                     placeholder={filter.placeholder}
-                     w="fit-content"
-                     p="2"
-                     onChange={(e) => searchProperties({[filter.queryName]: e.target.value})}>
-                      {filter?.items?.map((item) => (
-                         <option value={item.value} key={item.value}>{item.name}</option>
-                      ))}
-                    </Select>
-                </Box>
-             ))} 
-              <Flex flexDir='column'>
+
+      fetchData();
+    }
+  }, [searchTerm]);
+
+  return (
+    <Flex bg='gray.100' p='4' justifyContent='center' flexWrap='wrap'>
+      {filters?.map((filter) => (
+        <Box key={filter.queryName}>
+          <Select onChange={(e) => searchProperties({ [filter.queryName]: e.target.value })} placeholder={filter.placeholder} w='fit-content' p='2' >
+            {filter?.items?.map((item) => (
+              <option value={item.value} key={item.value}>
+                {item.name}
+              </option>
+            ))}
+          </Select>
+        </Box>
+      ))}
+      <Flex flexDir='column'>
         <Button onClick={() => setShowLocations(!showLocations)} border='1px' borderColor='gray.200' marginTop='2' >
           Search Location
         </Button>
@@ -96,7 +112,5 @@ const SeacrhFilters = () => {
         )}
       </Flex>
     </Flex>
-    )
+  );
 }
-
-export default SeacrhFilters
--- utils/fetchApi.js
+++ utils/fetchApi.js
@@ -1,3 +1,5 @@
+ 
+
 import axios from "axios";
 
 export const baseUrl = "https://bayut.p.rapidapi.com"
@@ -5,7 +7,7 @@ export const baseUrl = "https://bayut.p.rapidapi.com"
 export const fetchApi = async (url) => {
    const {data } = await axios.get((url), {
     headers: {
-        'X-RapidAPI-Key': '181b48e817mshbca2d08e1081fc2p1933a8jsn90c4b76730be',
+        'X-RapidAPI-Key': "181b48e817mshbca2d08e1081fc2p1933a8jsn90c4b76730be",
         'X-RapidAPI-Host': 'bayut.p.rapidapi.com'
       }
    })
