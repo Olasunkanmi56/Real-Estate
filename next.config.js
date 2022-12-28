42d88580532af5be9ba94d0fc60a3cb3e8f7ec50
Olasunkanmi56 <olasunkanmiolamide46@gmail.com>
2022-12-28 18:59:00 +0100

    first commit

---
+++ next.config.js
@@ -0,0 +1,14 @@
+/** @type {import('next').NextConfig} */
+// const nextConfig = {
+  
+//   swcMinify: true,
+ 
+// }
+
+module.exports = {
+  reactStrictMode: true,
+ images: {
+    domains: ["bayut-production.s3.eu-central-1.amazonaws.com"]
+  }
+
+}
